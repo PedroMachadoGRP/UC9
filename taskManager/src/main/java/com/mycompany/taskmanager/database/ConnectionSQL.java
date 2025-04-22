0f8f9339553877b4cfb2c82ee2e4c25e52d5cf5f
PedroMachadoGRP <143435304+PedroMachadoGRP@users.noreply.github.com>
2025-04-22 14:54:41 -0300

    Update ConnectionSQL.java

--- taskManager/src/main/java/com/mycompany/taskmanager/database/ConnectionSQL.java
+++ taskManager/src/main/java/com/mycompany/taskmanager/database/ConnectionSQL.java
@@ -4,10 +4,33 @@
  */
 package com.mycompany.taskmanager.database;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author PEDROMACHADODASILVA
  */
 public class ConnectionSQL {
-    
+    private static final Dotenv dotenv = Dotenv.load();
+        
+        private static Connection connection;
+        
+        private static final String URL = dotenv.get("DB_URL");
+        private static final String DB_USER = dotenv.get("DB_USER");
+        private static final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+        
+        public static Connection conect(){
+            try{
+                if(connection == null || connection.isClosed()){
+                    connection = DriverManager.getConnection(URL,DB_USER,DB_PASSWORD);
+                    //CreateTable();
+                    System.out.println("successfully connected");
+                }
+            } catch(SQLException error){
+                throw new RuntimeException("Error in database connection");
+            }
+            return connection;
+        }
 }
