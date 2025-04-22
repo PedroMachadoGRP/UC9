f24716e4a43b61c43f4e29f3fcaee22bed8bf8e2
Freliannys Lopez <lopezfreliannys@gmail.com>
2025-04-22 16:05:50 -0300

    Update TaskDAO.java

--- taskManager/src/main/java/com/mycompany/taskmanager/dao/TaskDAO.java
+++ taskManager/src/main/java/com/mycompany/taskmanager/dao/TaskDAO.java
@@ -9,5 +9,16 @@ package com.mycompany.taskmanager.dao;
  * @author PEDROMACHADODASILVA
  */
 public class TaskDAO {
+     public boolean deleteTask(int id){
+    String sql = "DELETE FROM tasks WHERE id = ?";
+ try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            return pstmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+            
+        } 
     
 }
